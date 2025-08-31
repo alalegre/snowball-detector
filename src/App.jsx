32885615dca8a7fc2e479467c5f09f986ba32440
Aldridge Alegre <almy.alegre@gmail.com>
2025-08-30 22:14:57 -0700

    Updated App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-import { Home } from './pages/Home'
+import { Home } from './pages/Home.jsx'
+import { BrowserRouter, Route, Routes } from "react-router"
+import { NotFound } from './pages/NotFound.jsx'
+import { TermPage } from './pages/TermPage.jsx'
 
 function App() {
     return (
-        <div className="App"></div>
+        <div className="App">
+            {/* Defines the overall structure of the web app */}
+            < BrowserRouter >
+                <Routes>
+                    <Route path='*' element={<NotFound />} />
+                    <Route path='/' element={<Home />} />
+                    <Route path='/:termID' element={<TermPage />} />
+                </Routes>
+            </BrowserRouter >
+        </div>
     )
 }
 
